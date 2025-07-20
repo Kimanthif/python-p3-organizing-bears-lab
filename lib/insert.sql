0f59331d298cc4eeaa3c580528452eadfd2fe919
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-07-20 22:04:05 +0300

    test done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+  (1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Aggressive', 1),
+  (2, 'Rowdy', 8, 'M', 'Black', 'Playful', 1),
+  (3, 'Tabitha', 5, 'F', 'Cinnamon', 'Calm', 1),
+  (4, 'Sergeant Brown', 12, 'M', 'Brown', 'Gruff', 1),
+  (5, 'Melissa', 6, 'F', 'Blonde', 'Friendly', 1),
+  (6, 'Grinch', 7, 'M', 'Green', 'Grumpy', 1),
+  (7, 'Wendy', 4, 'F', 'Golden', 'Gentle', 1),
+  (8, NULL, 3, 'F', 'White', 'Curious', 1);
